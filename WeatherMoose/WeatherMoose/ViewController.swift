ea08fee352c1fafc6074f6335c07948b5fccf3fe
Nick Matelli <nick14@gmail.com>
2019-08-07 20:19:25 -0700

    Hooked up services

--- WeatherMoose/WeatherMoose/ViewController.swift
+++ WeatherMoose/WeatherMoose/ViewController.swift
@@ -7,18 +7,60 @@ class ViewController: UIViewController {
     @IBOutlet weak var fLabel: UILabel!
     @IBOutlet weak var cLabel: UILabel!
 
-    let url = "https://samples.openweathermap.org/data/2.5/weather?zip=94040,us&appid=b6907d289e10d714a6e88b30761fae22"
+    let urlString = "https://api.openweathermap.org/data/2.5/weather"
+    let appId = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         fLabel.text = ""
         cLabel.text = ""
+        textField.becomeFirstResponder()
     }
 
     @IBAction func updateTapped(_ sender: Any) {
-        print("tapped")
+        let urlString = "\(self.urlString)?zip=\(textField.text ?? "")&appid=\(appId)"
+        print(urlString)
+        guard let url = URL(string: urlString) else {
+            presentAlert("Bad URL")
+            return
+        }
+        let request = URLRequest(url: url)
+        let session = URLSession(configuration: URLSessionConfiguration.default)
+        let task = session.dataTask(with: request) { [weak self] (data, response, error) in
+            DispatchQueue.main.async {
+                guard let data = data,
+                    let weatherResponse = try? JSONDecoder().decode(WeatherReponse.self, from: data) else {
+                    self?.presentAlert("Service error")
+                    return
+                }
+                self?.cLabel.text = "\(weatherResponse.cTemp)°C"
+                self?.fLabel.text = "\(weatherResponse.fTemp)°F"
+            }
+        }
+        task.resume()
+    }
+
+    func presentAlert(_ title: String) {
+        let alert = UIAlertController(title: title, message: "", preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(alertAction)
+        present(alert, animated: true)
     }
 
 }
 
+struct WeatherReponse: Codable {
+    let name: String
+    let main: WeatherMain
+    struct WeatherMain: Codable {
+        let temp: Double
+    }
+
+    var cTemp: Int { get {
+        return Int(main.temp - 273.15)
+    }}
+
+    var fTemp: Int { get {
+        return Int((main.temp - 273.15) * 9/5 + 32)
+    }}
+}
